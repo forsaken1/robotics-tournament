51d74bfd841c0e1228a0bfe466a949d84cd08964
forsaken1 <alexey2142@mail.ru>
2018-11-10 19:55:47 +1000

    add engine template

---
+++ lib/engine.py
@@ -0,0 +1,27 @@
+class Engine:
+    def __init__(self):
+        pass
+
+    def move(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def left(self):
+        pass
+
+    def right(self):
+        pass
+
+    def step(self):
+        pass
+
+    def step_back(self):
+        pass
+
+    def step_left(self):
+        pass
+
+    def step_right(self):
+        pass
