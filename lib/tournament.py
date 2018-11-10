ae04c64f9e18f1772f34faa760b4f731bd39263c
forsaken1 <alexey2142@mail.ru>
2018-11-11 03:25:18 +1000

    add Tournament#detect_first_task method

--- lib/tournament.py
+++ lib/tournament.py
@@ -2,12 +2,26 @@ from lib.tasks.start import Start
 from lib.tasks.black_line import BlackLine
 from lib.tasks.labyrinth import Labyrinth
 from lib.tasks.target import Target
+from lib.color import Color
+import pyb
 
 class Tournament:
     def __init__(self):
         self.tasks = [Start(), BlackLine(), Labyrinth(), Target()]
 
     def start(self):
+        # self.detect_first_task() # удалит первую задачу, если стартуем сразу на черной линии
         for task in self.tasks:
             while not task.passed:
                 task.run()
+
+    def detect_first_task(self):
+        measure_count = 0
+        color = Color()
+        for i in range(10):
+            if color.is_path():
+                measure_count++
+            pyb.delay(100)
+
+        if measure_count > 7:
+            self.tasks[1:]
