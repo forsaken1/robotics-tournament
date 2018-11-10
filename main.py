8adedadb7cf34b0558ddc35a03e5b6e4ae0e8ba3
forsaken1 <alexey2142@mail.ru>
2018-11-11 02:45:13 +1000

    move to examples

--- main.py
+++ main.py
@@ -1,30 +1,8 @@
 import pyb
 # import examples.sonar
+# import examples.color
+# import examples.line_detector
 import examples.engine
-
-## Color example
-
-# from sensors.color import *
-
-# color = Color(debug=True)
-# while True:
-#   print(color.is_path())
-#   pyb.delay(1000)
-
-## End color example
-
-
-## line detector example
-
-# from lib.line_detector import LineDetector
-
-# line_detector = LineDetector('Y11', debug = True)
-# while True:
-#     print(line_detector.is_path())
-#     pyb.delay(300)
-
-
-## end line detector example
 
 
 ## Start tournament
