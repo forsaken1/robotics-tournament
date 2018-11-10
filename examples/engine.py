434760f0ac036d549c01e5efe12a51de1798c8ad
Krivonos Ivan <devbackend@yandex.ru>
2018-11-11 03:05:21 +1000

    Engine

--- examples/engine.py
+++ examples/engine.py
@@ -1,14 +1,1 @@
-from lib.engine import Engine
-import pyb
-
-engine = Engine()
-
-while True:
-    engine.move()
-    pyb.delay(5000)
-
-    engine.stop()
-    pyb.delay(2000)
-
-    engine.back();
-    pyb.delay(5000)
+ÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿ
