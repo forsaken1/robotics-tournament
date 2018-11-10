74a0f38e6938526437c9c245bd069b67df833fd2
forsaken1 <alexey2142@mail.ru>
2018-11-11 03:33:41 +1000

    add improved algo for black line

---
+++ lib/tasks/black_line_improved.py
@@ -0,0 +1,15 @@
+from lib.tasks.black_line import BlackLine
+
+class BlackLineImproved(BlackLine):
+    """improved algo with line detectors"""
+
+    def __init__(self):
+        super()
+
+    def find_path(self):
+        if self.color.right_black():
+            self.engine.step_right()
+        elif self.color.left_black():
+            self.engine.step_left()
+        else
+            self.engine.step()
